ef0ac6e26c5cc2223b40002817ff93ee2210ed7f
Toni Van de Voorde <toni.vdv@gmail.com>
2014-08-28 10:57:41 +0200

    Add support for v201408

---
+++ src/main/java/eu/adlogix/com/google/api/ads/dfp/v201408/OrderStatementBuilderCreator.java
@@ -0,0 +1,24 @@
+package eu.adlogix.com.google.api.ads.dfp.v201408;
+
+import org.joda.time.DateTime;
+
+import com.google.api.ads.dfp.axis.utils.v201408.DateTimes;
+
+import eu.adlogix.com.google.api.ads.dfp.OrderStatementQueryFilter;
+import eu.adlogix.com.google.api.ads.dfp.StatementCondition;
+import eu.adlogix.com.google.api.ads.dfp.StatementQueryValue;
+
+public class OrderStatementBuilderCreator extends BaseStatementBuilderCreator {
+
+	public OrderStatementBuilderCreator withId(Long id, StatementCondition condition) {
+		where(OrderStatementQueryFilter.ID, new StatementQueryValue(id, condition));
+		return this;
+	}
+
+	public OrderStatementBuilderCreator withLastModifiedDateTime(DateTime lastModifiedDateTime,
+			StatementCondition condition) {
+		where(OrderStatementQueryFilter.LAST_MODIFIED_DATE_TIME, new StatementQueryValue(DateTimes.toDateTime(lastModifiedDateTime), condition));
+		return this;
+	}
+
+}
